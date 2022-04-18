55a3369c930d0713281b3af7d088159215b0a0c2
Narawich Youbua <narawich49@gmail.com>
2022-04-18 19:42:50 +0700

    All default set up done.

---
+++ CultureTravelSimple/CultureTravelSimple/Data/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CultureTravelSimple.Data
+{
+    public class User
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        public string? Is_Admin { get; set; }
+
+        [Required]
+        public string? Is_Ban { get; set; }
+    }
+}
---
+++ CultureTravelSimple/CultureTravelSimple/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CultureTravelSimple.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
